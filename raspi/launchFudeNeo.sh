31c0466d3b057cf10294147481e494ec2b532898
enket <tr.nayama@gmail.com>
2017-02-10 12:35:04 +0900

    add launch script for raspi

---
+++ raspi/launchFudeNeo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo env XDG_CONFIG_HOME=/home/pi/.config/ /usr/bin/terminator -f -b -e sudo -u pi env PATH=$PATH:/home/pi/.nvm/versions/node/v6.9.5/bin/ sh -c "kill `ps -ef | grep pm2 | grep -v grep | awk '{print $2;}'` ; pm2 start /home/pi/fudeVeatNeo-angular/app.js && pm2 monit"
