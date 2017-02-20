6c96a3e5eb2ba19b4518bb141b9d981cfa6dc0fe
enket <tr.nayama@gmail.com>
2017-02-20 15:03:42 +0900

    fix command string

--- raspi/launcher/launchFudeNeo.sh
+++ raspi/launcher/launchFudeNeo.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-sudo env XDG_CONFIG_HOME=/home/pi/.config/ /usr/bin/terminator -f -b -e sudo -u pi env PATH=$PATH:/home/pi/.nvm/versions/node/v6.9.5/bin/ sh -c /
-"kill `ps -ef | grep pm2 | grep -v grep | awk '{print $2;}'` ; /
-cd ~/fudeVeatNeo-angular/ && /
-npm install && /
-bower install && /
-pm2 start /home/pi/fudeVeatNeo-angular/app.js && /
-pm2 monit"
+sudo env XDG_CONFIG_HOME=/home/pi/.config/ /usr/bin/terminator -f -b -e sudo -u pi env PATH=$PATH:/home/pi/.nvm/versions/node/v6.9.5/bin/ sh -c "kill `ps -ef | grep pm2 | grep -v grep | awk '{print $2;}'` ; cd /home/pi/fudeVeatNeo-angular/ && npm install && bower install ; pm2 start /home/pi/fudeVeatNeo-angular/app.js && pm2 monit"
